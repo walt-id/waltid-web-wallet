2d533aaa5a2047ffb31227952fe9d1c988386f03
mikeplotean <mike@walt.id>
2023-08-09 16:16:16 +0300

    chore: login with wallet-address gives access to entire profile

--- src/main/kotlin/id/walt/service/account/WalletStrategy.kt
+++ src/main/kotlin/id/walt/service/account/WalletStrategy.kt
@@ -4,9 +4,7 @@ import id.walt.db.models.AccountWallets
 import id.walt.db.models.Accounts
 import id.walt.db.models.Wallets
 import id.walt.web.model.AddressLoginRequest
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.insertAndGetId
-import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.util.*
 
@@ -34,10 +32,9 @@ class WalletStrategy : AccountStrategy<AddressLoginRequest> {
 
     override fun authenticate(request: AddressLoginRequest): AuthenticatedUser = let {
         transaction {
-            (Accounts innerJoin Wallets).select { Wallets.address eq request.address }.takeIf {
-                it.any()
-            }?.first()?.let {
+            join(request.address).firstOrNull()?.let {
                 it[Accounts.id].value
+//                it[Wallets.address]
             }
         } ?: AccountsService.register(request).getOrThrow().id
     }.let { AuthenticatedUser(it, request.address) }
@@ -52,4 +49,10 @@ class WalletStrategy : AccountStrategy<AddressLoginRequest> {
             }.value
         }
     }
+
+    private fun join(address: String) = Accounts
+        .innerJoin(AccountWallets, onColumn = { Accounts.id }, otherColumn = { AccountWallets.account })
+        .innerJoin(Wallets, onColumn = { Wallets.id }, otherColumn = { AccountWallets.wallet }, additionalConstraint = {
+            Wallets.address eq address
+        }).selectAll()
 }
