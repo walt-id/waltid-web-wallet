ed41ba8bd6f02aeeffeef20aa76bb92e3f53ddf6
Severin Stampler <severin@walt.id>
2023-09-29 11:59:43 +0200

    update for missing c_nonce from mattr issuer

--- src/main/kotlin/id/walt/service/SSIKit2WalletService.kt
+++ src/main/kotlin/id/walt/service/SSIKit2WalletService.kt
@@ -234,7 +234,7 @@ class SSIKit2WalletService(accountId: UUID) : WalletService(accountId) {
         println(">>> Token response = success: ${tokenResp.isSuccess}")
 
         println("// receive credential")
-        var nonce = tokenResp.cNonce ?: throw IllegalArgumentException("No token nonce received!")
+        var nonce = tokenResp.cNonce
 
         println("Using issuer URL: ${parsedOfferReq.credentialOfferUri ?: parsedOfferReq.credentialOffer!!.credentialIssuer}")
         val credReq = CredentialRequest.forOfferedCredential(
