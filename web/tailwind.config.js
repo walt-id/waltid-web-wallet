328b5a51d6b8b453ee12e48f7c9d0dfa3d3b5792
waltkb <68587968+waltkb@users.noreply.github.com>
2023-10-05 14:39:24 +0200

    Removed tailwind warning

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -102,7 +102,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),
   ],
 }
