bdfef32734e80b1983e85d2a6dce9e81c13c6311
waltkb <68587968+waltkb@users.noreply.github.com>
2023-10-16 10:07:08 +0200

    Update to ktor 2.3.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
 
     /* -- KTOR -- */
 
-    val ktorVersion = "2.3.4"
+    val ktorVersion = "2.3.5"
     // Ktor server
     implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-auth-jvm:$ktorVersion")
