192aaed5238201e9709fa33d867d7e333001924f
mikeplotean <101570226+mikeplotean@users.noreply.github.com>
2023-10-20 23:30:37 +0300

    fix: reference public env (#38)

--- web/src/pages/settings/issuers/[issuer].vue
+++ web/src/pages/settings/issuers/[issuer].vue
@@ -19,7 +19,7 @@
                     </div> -->
                 </div>
                 <div class="flex flex-none items-center gap-x-4">
-                    <NuxtLink :to="issuerCredentials.issuer.uiEndpoint + credential.id + '&callback=' + config.issuerCallbackUrl"
+                    <NuxtLink :to="issuerCredentials.issuer.uiEndpoint + credential.id + '&callback=' + config.public.issuerCallbackUrl"
                               class="hidden rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block"
                               type="button">
                         Request credential offer
