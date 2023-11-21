53a3b63407170256c1d7654116c051911dbecfa3
waltkb <68587968+waltkb@users.noreply.github.com>
2023-11-21 04:57:55 +0100

    Set did listing transaction

--- src/main/kotlin/id/walt/web/controllers/DidController.kt
+++ src/main/kotlin/id/walt/web/controllers/DidController.kt
@@ -9,7 +9,9 @@ import io.github.smiley4.ktorswaggerui.dsl.route
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
+import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.JsonObject
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.dids() = walletRoute {
     route("dids", {
@@ -24,7 +26,7 @@ fun Application.dids() = walletRoute {
                 }
             }
         }) {
-            context.respond(getWalletService().listDids())
+            context.respond(getWalletService().run { transaction { runBlocking { listDids() } } })
         }
 
         route("{did}", {
